1357f23b8bb505cefb0c50f1b856d07e485dafef
SIDVJSINGH <sidvjsingh@gmail.com>
2023-06-27 02:50:30 +0530

    db initialize with sqlite

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # create the app
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///404_found.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
 # create the extension and initialize it
 db = SQLAlchemy(app)
 
@@ -16,9 +16,9 @@ class Contacts(db.Model):
     name, email, phone, message, srno, date_skey
     """
     # nullable is True by default
-    srno = db.Column(db.Integer, primary_key=True)
+    # srno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(12), nullable=False)
+    phone = db.Column(db.String(12),primary_key=True, nullable=False)
     email = db.Column(db.String(100), nullable=False)
     message = db.Column(db.String(255), nullable=False)
     date = db.Column(db.String(20), nullable=True)
@@ -51,16 +51,29 @@ def post2():
 def contact():
     if(request.method=='POST'):
         """ Add entry to DB """
-        name = request.form.get('name')
-        email = request.form.get('email')
-        phone = request.form.get('phone')
-        message = request.form.get('message')
+        # name = request.form.get('name')
+        # email = request.form.get('email')
+        # phone = request.form.get('phone')
+        # message = request.form.get('message')
+
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
 
         entry = Contacts(name=name, email=email, phone=phone, message=message, date=datetime.now())
-        db.session.add(entry)
-        db.session.commit()
+        # db.session.add(entry)
+        # db.session.commit()
 
+        # Push to the Database
+        try:
+            db.session.add(entry)
+            db.session.commit()
+            return redirect('/contact')
+        except:
+            return "Error sending info"
 
+    # contacts = Contacts.query.order_by(Contacts.date)
     return render_template("contact.html")
 
 
